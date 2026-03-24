8a514903872680d9778d78411ba2478ed65cd493
James Villanueva <jvillan314@gmail.com>
2026-03-24 01:35:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const hero = document.querySelector('.hero');
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
   const heroHeight = hero.offsetHeight;
-  const opacity = Math.min(scrollY / heroHeight, 1);
-  hero.style.backgroundColor = `rgba(255, 255, 255, ${1 - opacity})`;
 });
 
 
